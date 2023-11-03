469d08a9631a315ea1c0e9002e72b970ec5aee4c
Mathieu Barbin <mathieu.barbin@gmail.com>
2023-11-03 13:04:57 +0100

    use command-unix-for-opam
    - The local command-unix-for-opam was turned into an external package,
      which we can now depends on.

--- lib/command_unix_for_opam/src/command_unix_for_opam.ml
+++
@@ -1,3 +0,0 @@
-(* The substitution of the variables happens during the release packaging
-   process, which invokes `dune subst` at the appropriate time. *)
-let run cmd = Command_unix.run ~version:"%%VERSION%%" ~build_info:"%%VCS_COMMIT_ID%%" cmd
--- lib/command_unix_for_opam/src/command_unix_for_opam.mli
+++
@@ -1,7 +0,0 @@
-(** This util library is meant to improve compatibility of core commands with the
-    opam distribution process. *)
-
-(** [run cmd] is the same as [Command_unix.run cmd] but adds extra parameters so
-    that invocations of the command with [-version] outputs the package
-    version. *)
-val run : Command.t -> unit
