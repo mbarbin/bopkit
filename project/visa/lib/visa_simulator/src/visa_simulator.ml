722287e5e72df04730b3ef5b14a13c92d6d9c96f
Mathieu Barbin <mathieu.barbin@gmail.com>
2024-11-10 00:11:11 +0100

    pp-log.err and .cli renames

--- project/visa/lib/visa_simulator/src/visa_simulator.ml
+++ project/visa/lib/visa_simulator/src/visa_simulator.ml
@@ -265,7 +265,7 @@ let main =
          (Param.validated_string (module Fpath))
          ~docv:"FILE"
          ~doc:"assembler program to execute"
-     and () = Err_cli.set_config ()
+     and () = Pp_log_cli.set_config ()
      and config = Config.arg in
      let program = Parsing_utils.parse_file_exn (module Visa_syntax) ~path in
      let visa_simulator = create ~config ~program in
