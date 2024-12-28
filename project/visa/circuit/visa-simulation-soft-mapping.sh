91e30b2bbe2652fdee82536739dbab7e2f0bac52
Mathieu Barbin <mathieu.barbin@gmail.com>
2024-12-28 18:05:27 +0100

    Revert "Construct pipe-stdout got broken at some point"
    This reverts commit 5031dbfa0bb6b67348e6b22875f6cd09b988ef1d.

--- project/visa/circuit/visa-simulation-soft-mapping.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-bopkit simu visa.bop --output-only-on-change | \
-bopkit digital-calendar map-raw-input | \
-bopkit digital-calendar display --no-output
--- project/visa/circuit/visa-simulation.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-bopkit simu visa.bop --output-only-on-change | \
-bopkit simu calendar-output.bop -p | \
-bopkit digital-calendar display --no-output
