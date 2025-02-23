2cc4642b820151e97a6b356e19d1e2e07fedcbf0
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-02-23 20:34:33 +0100

    Rename _command to _cli

--- project/subleq/bin/main.ml
+++ project/subleq/bin/main.ml
@@ -1,3 +1,1 @@
-let () =
-  Cmdlang_cmdliner_runner.run Subleq_command.main ~name:"subleq" ~version:"%%VERSION%%"
-;;
+let () = Cmdlang_cmdliner_runner.run Subleq_cli.main ~name:"subleq" ~version:"%%VERSION%%"
