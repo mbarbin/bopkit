a87338f16c810f9759afb58c1d034fb2cb823f29
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-03-19 12:56:29 +0100

    Fix warning reported by dune's unused-libs build target
    - For tests, added an empty test to pin the intended dependency
    - For actual unused dependency, remove it
    
    At the moment this project uses .headache.dirs configuration for the license
    headers so this change updated it. We are working on simplifying that part of
    the setup but this repo hasn't been migrated to the new scheme yet. The new
    scheme relies on tracked files and removes the need for the headache.dirs
    config, but that part is left as future work.

---
+++ lib/bopkit_memory/test/test__bopkit_memory.ml
@@ -0,0 +1,14 @@
+(*********************************************************************************)
+(*  bopkit: An educational project for digital circuits programming              *)
+(*  SPDX-FileCopyrightText: 2007-2025 Mathieu Barbin <mathieu.barbin@gmail.com>  *)
+(*  SPDX-License-Identifier: MIT                                                 *)
+(*********************************************************************************)
+
+(* This is to silence `dune build @unused-libs` and keeping intended deps. *)
+open! Bopkit_memory
+
+let%expect_test "empty" =
+  ();
+  [%expect {||}];
+  ()
+;;
---
+++ project/visa/lib/visa_parser/test/test__visa_parser.mli
@@ -0,0 +1,5 @@
+(*_********************************************************************************)
+(*_  bopkit: An educational project for digital circuits programming              *)
+(*_  SPDX-FileCopyrightText: 2007-2025 Mathieu Barbin <mathieu.barbin@gmail.com>  *)
+(*_  SPDX-License-Identifier: MIT                                                 *)
+(*_********************************************************************************)
